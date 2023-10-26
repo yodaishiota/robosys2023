1446bfb1faf19cc4d3cc56befeabc82f93121e93
Yodai Shiota <shioyan0809@gmail.com>
2023-10-26 12:55:26 +0900

    rm yamada.bash

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yodai Shiota
+# SPDX-License-Identifire BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+:
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng  ${LINENO}
 
-[ "$out" = 14 ]
+[ "res" = 0 ] && echo OK
+exit $res
 
