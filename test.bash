2f86de083e5a0c7caeb0631559f3fc412aa5e7bc
Yodai Shiota <shioyan0809@gmail.com>
2023-11-26 22:23:08 +0900

    check opration and add test item

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Yodai Shiota
 # SPDX-License-Identifire BSD-3-Clause
 
@@ -11,6 +11,14 @@ res=0
 
 out=$(seq 5 | ./plus_stdin)
 [ "${out}" = 15 ] || ng  ${LINENO}
+
+out=$(echo あ | ./plus_stdin)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　
+out=$(echo | ./plus_stdin)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "res" = 0 ] && echo OK
 exit $res
