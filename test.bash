442a598e72c592e83471f9ae3fe112a4cad134fe
Yodai Shiota <shioyan0809@gmail.com>
2023-11-26 22:16:44 +0900

    improve test

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yodai Shiota
+# SPDX-License-Identifire BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 15 ] || ng  ${LINENO}
+
+[ "res" = 0 ] && echo OK
+exit $res
