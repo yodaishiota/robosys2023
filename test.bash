a08982d5b5c3e4ffeb656074737eb54bb4af8617
Yodai Shiota <shioyan0809@gmail.com>
2023-11-28 16:46:23 +0900

    add test

--- test.bash
+++ test.bash
@@ -3,22 +3,34 @@
 # SPDX-License-Identifire BSD-3-Clause
 
 ng () {
-        echo NG at Line $1
-        res=1
+      echo NG at Line $1
+      res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng  ${LINENO}
+out=$(seq 10 | ./plus)
+[ "${out}" = 55 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
-    　
-out=$(echo  | ./plus)
+
+out=$(echo a | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-[ "res" = 0 ] && echo OK
+out=$(echo A | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo @ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
